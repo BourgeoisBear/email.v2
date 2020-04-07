63978285de94a0a49b34e79df432e6d68fd526f1
Jason Stewart <support@eggplantsd.com>
2020-04-07 18:51:06 -0400

    - variadic MSG parameter for `SMTPClientConfig.SimpleSend()`

--- smtp_client_config.go
+++ smtp_client_config.go
@@ -1,21 +1,20 @@
 package email
 
 import (
-	"fmt"
-	"time"
-	"errors"
-	"strings"
 	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
 
-	"net"
 	"crypto/tls"
+	"net"
 
-	"os"
 	"log"
+	"os"
 )
 
 type SMTPClientMode uint8
-
 
 const (
 	ModeUNENCRYPTED SMTPClientMode = 0
@@ -23,16 +22,16 @@ const (
 	ModeFORCETLS    SMTPClientMode = 2
 )
 
-
 var errInvalidMode error = errors.New("Valid SMTPClientModes are: UNENCRYPTED, STARTTLS, or FORCETLS")
-
 
 func (MODE *SMTPClientMode) UnmarshalJSON(V []byte) (E error) {
 
 	var STR string
 
 	E = json.Unmarshal(V, &STR)
-	if E != nil { return }
+	if E != nil {
+		return
+	}
 
 	STR = strings.ToUpper(strings.TrimSpace(STR))
 
@@ -51,23 +50,21 @@ func (MODE *SMTPClientMode) UnmarshalJSON(V []byte) (E error) {
 	return
 }
 
-
 type SMTPClientConfig struct {
-	Server        string
-	Port          uint16
-	Username      string
-	Password      string
-	Mode          SMTPClientMode
-	TimeoutMsec   uint32
-	Proto         string  // dial protocol: `tcp`, `tcp4`, or `tcp6`; defaults to `tcp`
-	SMTPLog       string  // path to SMTP log: complete filepath, "-" for STDOUT, or empty to disable SMTP logging
+	Server      string
+	Port        uint16
+	Username    string
+	Password    string
+	Mode        SMTPClientMode
+	TimeoutMsec uint32
+	Proto       string // dial protocol: `tcp`, `tcp4`, or `tcp6`; defaults to `tcp`
+	SMTPLog     string // path to SMTP log: complete filepath, "-" for STDOUT, or empty to disable SMTP logging
 }
 
+func (CFG SMTPClientConfig) SimpleSend(MSG ...*Email) (E error) {
 
-func (CFG SMTPClientConfig) SimpleSend(MSG *Email) (E error) {
-
-	iAuth     := LoginAuth(CFG.Username, CFG.Password)
-	pTLSCfg   := TLSConfig(CFG.Server)
+	iAuth := LoginAuth(CFG.Username, CFG.Password)
+	pTLSCfg := TLSConfig(CFG.Server)
 	DIAL_ADDR := fmt.Sprintf("%s:%d", CFG.Server, CFG.Port)
 
 	if len(CFG.Proto) == 0 {
@@ -84,13 +81,15 @@ func (CFG SMTPClientConfig) SimpleSend(MSG *Email) (E error) {
 	var iConn net.Conn
 	var pTLSCfgClient *tls.Config = nil
 
-	switch( CFG.Mode ) {
+	switch CFG.Mode {
 
 	case ModeSTARTTLS:
 
 		// [1]: open an unencrypted network connection
 		iConn, E = pDialer.Dial(CFG.Proto, DIAL_ADDR)
-		if E != nil { return }
+		if E != nil {
+			return
+		}
 
 		// negotiate TLS in SMTP session
 		pTLSCfgClient = pTLSCfg
@@ -99,13 +98,17 @@ func (CFG SMTPClientConfig) SimpleSend(MSG *Email) (E error) {
 
 		// [1]: open a TLS-secured network connection
 		iConn, E = tls.DialWithDialer(pDialer, CFG.Proto, DIAL_ADDR, pTLSCfg)
-		if E != nil { return }
+		if E != nil {
+			return
+		}
 
 	case ModeUNENCRYPTED:
 
 		// [1]: open an unencrypted network connection
 		iConn, E = pDialer.Dial(CFG.Proto, DIAL_ADDR)
-		if E != nil { return }
+		if E != nil {
+			return
+		}
 
 	default:
 
@@ -123,27 +126,45 @@ func (CFG SMTPClientConfig) SimpleSend(MSG *Email) (E error) {
 		if CFG.SMTPLog == "-" {
 			FILE = os.Stdout
 		} else {
-			FILE, E = os.OpenFile(CFG.SMTPLog, os.O_WRONLY | os.O_CREATE | os.O_APPEND, 0660)
-			if E != nil { return }
+			FILE, E = os.OpenFile(CFG.SMTPLog, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0660)
+			if E != nil {
+				return
+			}
 		}
 
-		fnTextprotoCreate = TextprotoLogged(log.New(FILE, "", log.Ltime | log.Lmicroseconds), true)
+		fnTextprotoCreate = TextprotoLogged(log.New(FILE, "", log.Ltime|log.Lmicroseconds), true)
 	}
 
-  // COMMS TIMEOUT
-  if TIMEOUT_DURATION > 0 {
-
-	  E = iConn.SetDeadline(time.Now().Add(TIMEOUT_DURATION))
-	  if E != nil { return }
+	// COMMS TIMEOUT
+	if TIMEOUT_DURATION > 0 {
+		E = iConn.SetDeadline(time.Now().Add(TIMEOUT_DURATION))
+		if E != nil {
+			return
+		}
 	}
 
 	// [2]: ESTABLISH SMTP SESSION
 	SESS, E := NewClient(iConn, iAuth, CFG.Server, pTLSCfgClient, fnTextprotoCreate)
-	if E != nil { return }
+	if E != nil {
+		return
+	}
 
 	// [3]: SEND MESSAGE(S)
-	E = SESS.Send(MSG)
-	if E != nil { return }
+	for ix := range MSG {
+
+		// COMMS TIMEOUT
+		if TIMEOUT_DURATION > 0 {
+			E = iConn.SetDeadline(time.Now().Add(TIMEOUT_DURATION))
+			if E != nil {
+				return
+			}
+		}
+
+		E = SESS.Send(MSG[ix])
+		if E != nil {
+			return
+		}
+	}
 
 	// [4]: CLOSE SMTP SESSION WHEN FINISHED SENDING
 	// NOTE: this also closes the underlying network connection
